686299fba538ccf612c76195de1f677bda569da3
Ben Sudbury <ben@pvlighthouse.com.au>
2020-07-27 11:53:18 +1000

    Add timing to example

--- examples/findFilesSinceDate.js
+++ examples/findFilesSinceDate.js
@@ -5,17 +5,18 @@
  * Time: 2:18 PM
  * To change this template use File | Settings | File Templates.
  */
+var start = new Date()
+    
+var finder = require("../dist/node-find-files.js");
 
-    var finder = require("../dist/node-find-files.js");
+console.log(finder);
+var d = new Date()
+d.setDate(d.getDate() - 1);
 
-    console.log(finder);
-    var d = new Date()
-    d.setDate(d.getDate() - 1);
-
-    var finder = new finder({
-        rootFolder : "/Users",
-        fileModifiedDate : d
-    });
+var finder = new finder({
+    rootFolder : "/Users",
+    fileModifiedDate : d
+});
 
 
 //    //  Alternate Usage to acheive the same goal, but you can use any of the properties of the fs.stat object or the path to do your filtering
@@ -27,16 +28,18 @@
 //    });
 
 
-    finder.on("match", function(strPath, stat) {
-        console.log(strPath + " - " + stat.mtime);
-    })
-    finder.on("complete", function() {
-        console.log("Finished")
-    })
-    finder.on("patherror", function(err, strPath) {
-        console.log("Error for Path " + strPath + " " + err)
-    })
-    finder.on("error", function(err) {
-        console.log("Global Error " + err);
-    })
-    finder.startSearch();
+finder.on("match", function(strPath, stat) {
+    console.log(strPath + " - " + stat.mtime);
+})
+finder.on("complete", function() {
+    console.log("Finished");
+    var end = new Date() - start
+    console.info('Execution time: %dms', end);
+})
+finder.on("patherror", function(err, strPath) {
+    console.log("Error for Path " + strPath + " " + err)
+})
+finder.on("error", function(err) {
+    console.log("Global Error " + err);
+})
+finder.startSearch();
